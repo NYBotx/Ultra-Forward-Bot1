313270f7dabb18fa52663875142a51680f313d4e
NYBotx <167092531+NYBotx@users.noreply.github.com>
2024-11-25 13:13:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Nyfwbot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '2103299862').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002421238378'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "nyyybots") 
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "@nyyybots") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
     
