7c54344d003bb658121554aba6c5a50be0077d9f
NYBotx <167092531+NYBotx@users.noreply.github.com>
2024-11-25 12:16:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ from os import environ
 # For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = environ.get("API_ID", "13963336")
+    API_HASH = environ.get("API_HASH", "a144d1e22ef0b29738e8c00713d02678")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7792954183:AAHvrKQ984EDHRG6vCLTYp_WoHDHQveOXgE") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Nischay999:Nischay999@cluster0.5kufo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Nyfwbot")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '2103299862').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002421238378'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     PORT = environ.get('PORT', '8080')
     
 #Dont Remove My Credit @Silicon_Bot_Update 
